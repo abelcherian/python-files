5dfa1c79bc3fa8be547c8fc5c674145c21d462e0
abelcherian <abel1cherian@gmail.com>
2024-10-01 15:40:07 +0530

    Add files via upload
    comments added

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
+'''abelcherian'''
 student_name=input("enter the student name")
 roll_number=int(input("enter the roll number of student"))
 cgpa= float(input("enter cgpa here:"))
 print("your name is",student_name)
 print("your roll number is",roll_number+1)
 print("your cgpa is",cgpa*10,"%")
+
